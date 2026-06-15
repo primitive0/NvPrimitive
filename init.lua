63f7ae163ff7c54221e83dd73dad0d7cf623b6d6
primitive0 <94762195+primitive0@users.noreply.github.com>
2026-06-15 17:59:27 +0300

    Make Neovide font smaller

--- init.lua
+++ init.lua
@@ -34,7 +34,7 @@ vim.keymap.set('n', 'N', 'Nzz')
 
 -- Neovide
 if vim.g.neovide then
-  vim.o.guifont = 'Iosevka:h12'
+  vim.o.guifont = 'Iosevka:h11'
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_scroll_animation_length = 0
   vim.g.neovide_scroll_animation_far_lines = 0
