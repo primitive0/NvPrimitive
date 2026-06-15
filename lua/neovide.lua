03cdced55b9f5557f60ac8fa09ac1d89e4c4f831
primitive0 <94762195+primitive0@users.noreply.github.com>
2026-06-15 18:04:20 +0300

    Remove unused files

--- lua/neovide.lua
+++
@@ -1,7 +0,0 @@
-if vim.g.neovide then
-  vim.o.guifont = 'Iosevka:h12'
-  vim.g.neovide_cursor_animation_length = 0
-  vim.g.neovide_scroll_animation_length = 0
-  vim.g.neovide_scroll_animation_far_lines = 0
-  vim.g.neovide_hide_mouse_when_typing = true
-end
