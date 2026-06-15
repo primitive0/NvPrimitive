fe52df8227d35557142ece46cadc3d30d32a9f0e
primitive0 <94762195+primitive0@users.noreply.github.com>
2026-06-15 17:58:14 +0300

    Use Neogit instead of vim-fugitive

--- init.lua
+++ init.lua
@@ -57,7 +57,7 @@ vim.api.nvim_create_autocmd('PackChanged', {
 vim.pack.add {
   'https://github.com/miikanissi/modus-themes.nvim',
   'https://github.com/stevearc/conform.nvim',
-  'https://github.com/tpope/vim-fugitive',
+  'https://github.com/neogitorg/neogit',
   'https://github.com/lewis6991/gitsigns.nvim',
   'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/ibhagwan/fzf-lua',
