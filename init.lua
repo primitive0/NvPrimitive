470097e4c3a83bba6b5be8bdcba02ec44b63adf2
primitive0 <94762195+primitive0@users.noreply.github.com>
2026-06-18 20:17:39 +0300

    Change picker layout

--- init.lua
+++ init.lua
@@ -68,15 +68,44 @@ vim.opt.keymap = 'russian-jcukenwin'
 vim.opt.iminsert = 0
 vim.opt.imsearch = 0
 
--- Snacks picker
+-- Picker
 require('snacks').setup {
   picker = {
     enabled = true,
+    layout = {
+      preview = 'main',
+      layout = {
+        box = 'vertical',
+        backdrop = false,
+        width = 0,
+        height = 0.4,
+        position = 'bottom',
+        border = 'top',
+        title = ' {title} {live} {flags}',
+        title_pos = 'left',
+        { win = 'input', height = 1, border = 'bottom' },
+        {
+          box = 'horizontal',
+          { win = 'list', border = 'none' },
+          {
+            win = 'preview',
+            title = '{preview}',
+            width = 0.6,
+            border = 'left',
+          },
+        },
+      },
+    },
     win = {
       input = {
         keys = {
           ['<Esc>'] = { 'close', mode = { 'n', 'i' } },
         },
+      },
+    },
+    sources = {
+      files = {
+        layout = { hidden = { 'preview' } },
       },
     },
   },
