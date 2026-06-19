b0c78134a737c6913360b3816e9b69013408fafd
primitive0 <94762195+primitive0@users.noreply.github.com>
2026-06-19 11:28:54 +0300

    Do not set vim.o.guifont
    vim.o.guifont format does not support configuring font features (these
    are needed to control font ligatures).

--- init.lua
+++ init.lua
@@ -25,7 +25,6 @@ vim.g.maplocalleader = ' '
 
 -- Neovide
 if vim.g.neovide then
-  vim.o.guifont = 'Iosevka:h11'
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_scroll_animation_length = 0
   vim.g.neovide_scroll_animation_far_lines = 0
